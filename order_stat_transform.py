dd8980da06d76af66e8909f95e0c39a9c58ae5e5
Jonne Saleva <jsaleva@isi.edu>
2022-10-08 09:31:37 -0700

    Fix order_stat_transform.py

--- order_stat_transform.py
+++ order_stat_transform.py
@@ -6,11 +6,13 @@ import click
 
 @click.command()
 @click.option("-s", "--separator", default=" ")
+@click.option("--split-incoming-line", is_flag=True)
 @click.option("--include-key", is_flag=True, help="Include 1:1 encryption key in output")
-def main(separator: str, include_key: bool) -> None:
+def main(separator, split_incoming_line, include_key):
     for line in click.get_text_stream("stdin"):
-        freqs = {k: str(v) for k, v in h.gen_order_stat_map(line).items()}
-        encrypted = h.encrypt(line, freqs, sep=separator)
+        chars = line.split(separator) if split_incoming_line else line
+        freqs = {k: str(v) for k, v in h.gen_order_stat_map(chars).items()}
+        encrypted = h.encrypt(chars, freqs, sep=separator)
         click.echo(f"{encrypted}\t{freqs}" if include_key else encrypted, nl=True)
 
 
