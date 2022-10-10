1c05e8393bc3486a9252307c030dfb4fa32af39e
Jonne Saleva <jsaleva@isi.edu>
2022-10-10 11:02:23 -0700

    Model as arg to translate_from_stdin.sh

--- translate_from_stdin.sh
+++ translate_from_stdin.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
+MODEL_NAME=${1:-nadaformer}
+
 cat - | tr " " "_" | sed "s/./& /g" | sed "s/\s$//g" |\
  python order_stat_transform.py --split-incoming-line |\
  fairseq-interactive \
     ./data-bin/gutenberg-data/multilingual/ \
     --source-lang freq --target-lang multi \
-    --path ./experiments/freq2multilingual/train/nadaformer/checkpoints/checkpoint_best.pt \
+    --path ./experiments/freq2multilingual/train/$MODEL_NAME/checkpoints/checkpoint_best.pt \
     --seed 1917 --beam 100 --max-tokens 10000 |\
  rg "^H-" | cut -f3 | tr -d " " | tr "_" " "
