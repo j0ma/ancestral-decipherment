d4220ec523d405ee8bf14ebd4d4586c3fe26303a
Jonne Saleva <jsaleva@isi.edu>
2022-10-10 10:59:06 -0700

    sbatch train scripts and configs in separate files

---
+++ sbatch_train_transformer_halfLR.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=16GB
+#SBATCH --gres=gpu:v100:1
+#SBATCH --account=jonmay_231
+#SBATCH --mail-user jonnesaleva@brandeis.edu
+#SBATCH --mail-type ALL
+#SBATCH --job-name halfLR_freq2multi
+#SBATCH --requeue
+#SBATCH --time 48:00:00
+
+module purge
+module load nvidia-hpc-sdk
+module load gcc/8.3.0
+module load cuda/11.3.0
+
+export KOE=freq2multilingual
+export MALLI=nadaformer_halfLR_30epoch
+export SRC=freq
+export TGT=multi
+export CFG_FILE=transformer_config_halfLR.sh
+
+./train_transformer.sh "${KOE}" "${MALLI}" "${SRC}" "${TGT}" "${CFG_FILE}"
